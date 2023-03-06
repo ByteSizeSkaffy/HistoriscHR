9fc2a8d15d54645db3c2e5c3190b4792f34d1783
ByteSizeSkaffy <72089975+ByteSizeSkaffy@users.noreply.github.com>
2023-03-06 20:32:32 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import image1 from './images/maps/1936.jpg';
+import image2 from './images/maps/1920.jpg';
+import image3 from './images/maps/1903.jpg';
+import './app.css';
+
+function App() { 
+  return (
+    <div className="App">
+      <div className="mapgrid3">
+        <div className="mapgrid2">
+        <div className="mapgrid">
+
+        <img className="last" src={image1} alt="loading...." />
+        <div className='maptitle'> 1936</div>
+        
+        </div>
+        <img className="main" src={image2} alt="loading...." />
+        <div className='maptitle'> 1920</div>
+        
+        </div>
+        <img className="next" src={image3} alt="loading...." />
+        <div className='maptitle'> 1903</div>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
