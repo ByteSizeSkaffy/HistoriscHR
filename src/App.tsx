7b77408019e9aed7b5778cbf53c768401b65dc61
ByteSizeSkaffy <72089975+ByteSizeSkaffy@users.noreply.github.com>
2023-03-11 16:47:39 +0100

    added multiple sized screen support except mobile
    changed front end slightly so the animations dont overflow their respective backgrounds.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
-import React from 'react';
+import React,  {useState} from 'react';
 import image1 from './images/maps/1936.jpg';
 import image2 from './images/maps/1920.jpg';
 import image3 from './images/maps/1903.jpg';
 import './app.css';
 
-function App() { 
+
+function App() {
+  
   return (
     <div className="App">
+      <div className="mapgridbackground">
       <div className="mapgrid3">
         <div className="mapgrid2">
         <div className="mapgrid">
 
-        <img className="last" src={image1} alt="loading...." />
+        <img className="firstmap" src={image1} alt="loading...." />
         <div className='maptitle'> 1936</div>
         
         </div>
-        <img className="main" src={image2} alt="loading...." />
+        <img className="middlemap" src={image2} alt="loading...." />
         <div className='maptitle'> 1920</div>
         
         </div>
-        <img className="next" src={image3} alt="loading...." />
+        <img className="lastmap" src={image3} alt="loading...." />
         <div className='maptitle'> 1903</div>
-      
+
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
