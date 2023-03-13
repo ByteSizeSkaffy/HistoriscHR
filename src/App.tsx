347702d65ddc592b697bc70359760e45087a297b
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2023-03-13 10:44:40 +0100

    Slider

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,34 @@ import image2 from './images/maps/1920.jpg';
 import image3 from './images/maps/1903.jpg';
 import './app.css';
 
+function bar(){
+  const helo="helo"
+}
+
 function App() { 
   return (
     <div className="App">
-      <div className="mapgrid3">
-        <div className="mapgrid2">
-        <div className="mapgrid">
+      <div className="mapgrid4">
+        <div className="mapgrid3">
+          <div className="mapgrid2">
+            <div className="mapgrid">
 
-        <img className="last" src={image1} alt="loading...." />
-        <div className='maptitle'> 1936</div>
-        
+              <img className="last" src={image1} alt="loading...." />
+              <div className='maptitle'> 1936</div>
+            
+            </div>
+            <img className="main" src={image2} alt="loading...." />
+            <div className='maptitle'> 1920</div>
+          
+          </div>
+          <img className="next" src={image3} alt="loading...." />
+          <div className='maptitle'> 1903</div>
         </div>
-        <img className="main" src={image2} alt="loading...." />
-        <div className='maptitle'> 1920</div>
-        
-        </div>
-        <img className="next" src={image3} alt="loading...." />
-        <div className='maptitle'> 1903</div>
-      
+      <img className="main" src={image1} alt="loading...." id="test"/>
+      <h3 className='maptitle'> bartest</h3>
+      <div className="slidecontainer">
+        <input type="range" min="1" max="5" className="slider" id="myRange"/>
+      </div>
       </div>
     </div>
   );
