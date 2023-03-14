eac5503bc5c3a44b221b66c72949c9589c45210b
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2023-03-14 10:04:23 +0100

    bar

--- src/bar.tsx
+++ src/bar.tsx
@@ -1,7 +1,7 @@
 import image1 from './images/maps/1936.jpg';
 import image2 from './images/maps/1920.jpg';
 import image3 from './images/maps/1903.jpg';
-import {useEffect, useRef,useState} from "react";
+import {useRef,useState} from "react";
 import './app.css';
 
 function Bar(){
