06f06c6d114866c130836c4293fccaae0ba81bcd
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2023-03-24 09:41:49 +0100

    Manual Main Merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,61 @@
-import React, { useRef,useState } from 'react';
+import React,  {useState} from 'react';
 import image1 from './images/maps/1936.jpg';
 import image2 from './images/maps/1920.jpg';
 import image3 from './images/maps/1903.jpg';
 import './app.css';
 
 
-function App() { 
-  return (
-    <div className="App">
-      <div className="mapgrid4">
+function App() {
+  const [map, setMap] = useState(false);
+  const [map2, setMap2] = useState(false);
+  const [map3, setMap3] = useState(false);
+  if(map === true){
+    return (
+      <div className="App">
+        <div className="mapgridbackground">
         <div className="mapgrid3">
           <div className="mapgrid2">
-            <div className="mapgrid">
-
-              <img className="last" src={image1} alt="loading...." />
-              <div className='maptitle'> 1936</div>
-            
-            </div>
-            <img className="main" src={image2} alt="loading...." />
-            <div className='maptitle'> 1920</div>
+          <div className="mapgrid">
+  
+          <img className="firstmapclicked" src={image1} onClick ={()=>setMap(!map)} alt="loading...." />
+          <div className='maptitle'> 1936</div>
           
           </div>
-          <img className="next" src={image3} alt="loading...." />
+          <img className="middlemap" src={image2} alt="loading...." />
+          <div className='maptitle'> 1920</div>
+          
+          </div>
+          <img className="lastmap" src={image3} alt="loading...." />
           <div className='maptitle'> 1903</div>
+  
         </div>
       </div>
     </div>
-  );
+    );
+  }
+  else{
+  return (
+    <div className="App">
+      <div className="mapgridbackground">
+      <div className="mapgrid3">
+        <div className="mapgrid2">
+        <div className="mapgrid">
+
+        <img className="firstmap" src={image1} onClick ={()=>setMap(!map)} alt="loading...." />
+        <div className='maptitle'> 1936</div>
+        </div>
+
+        <img className="middlemap" src={image2} alt="loading...." />
+        <div className='maptitle'> 1920</div>
+        </div>
+
+        <img className="lastmap" src={image3} alt="loading...." />
+        <div className='maptitle'> 1903</div>
+        </div>
+      </div>
+    </div>
+    );
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Bar from "./bar";
-import Slide from "./Slide"
 import "@picocss/pico";  
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-
-
 root.render(
   <React.StrictMode>
     <App />
-    <Slide/>
   </React.StrictMode>
 );
 
