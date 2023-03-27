f0265bf640f39c19731c31fbb5f0846dadd31254
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2023-03-27 19:50:32 +0200

    ImageMarker

---
+++ src/ImageMarker.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useState} from 'react';
+
+type MarkerProps={
+    ImageOld:string;
+    ImageMiddle:string;
+    ImageNew:string;
+}
+
+const Marker: React.FC<MarkerProps> = ({ImageOld, ImageMiddle, ImageNew}) => {
+
+    return(
+        <div></div>
+    )
+}
+
+export default Marker;
+export type {};
